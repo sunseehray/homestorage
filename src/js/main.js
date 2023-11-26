50db17a649f26837021d20cc0bd81b7537d86df8
sunseehray <sabtirazona@byui.edu>
2023-11-26 23:38:43 +0800

    mostly css

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import { loadHeaderFooter } from './utils.mjs';
+
+const headerPath = '../partials/header.html';
+const footerPath = '../partials/footer.html';
+loadHeaderFooter(headerPath, footerPath);
+
+console.log(headerPath);
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -15,9 +15,55 @@ export function setLocalStorage(key, data) {
 }
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
-  qs(selector).addEventListener("touchend", (event) => {
+  qs(selector).addEventListener('touchend', (event) => {
     event.preventDefault();
     callback();
   });
-  qs(selector).addEventListener("click", callback);
+  qs(selector).addEventListener('click', callback);
+}
+
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
+export function renderListWithTemplate(templateFn, parentElement, list, position = 'afterbegin', clear = false) {
+  //clear parent element if clear === true  
+  if (clear === true) {
+      parentElement.innerHTML = '';
+    }  
+    const htmlStrings = list.map(templateFn);
+    parentElement.insertAdjacentHTML(position, htmlStrings.join(''));
+   
+
+}
+export function renderWithTemplate(template, parentElement, data, position = 'afterbegin', callback) {
+  parentElement.insertAdjacentHTML(position, template);
+
+  if (callback) {
+      callback(data);
+    }     
+}
+
+export async function loadTemplate(path) {
+
+  const html = await fetch(path);
+  const template = await html.text();
+  return template;
+}
+
+export async function loadHeaderFooter(headerPath, footerPath) {
+  // load the header and footer templates in from partials
+  const headerTemplate = await loadTemplate(headerPath);
+  const footerTemplate = await loadTemplate(footerPath);
+
+  //grab the header and footer elements out of the DOM
+  const headerId = document.querySelector('#main-header');
+  const footerId = document.querySelector('#main-footer');
+
+  // render the header and footer
+  renderWithTemplate(headerTemplate, headerId);
+  renderWithTemplate(footerTemplate, footerId);
 }
