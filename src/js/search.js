13f889b53ba9329fd8614a8d2cd7b433a20aa39b
sunseehray <sabtirazona@byui.edu>
2023-11-28 23:36:52 +0800

    template improved but still need styling

--- src/js/search.js
+++ src/js/search.js
@@ -10,29 +10,38 @@ loadHeaderFooter(headerPath, footerPath);
 
 const dataSource = new ExternalServices();
 
-document.querySelector("#search-button").addEventListener("click", async (e) => {
-  e.preventDefault();
-  const data = await dataSource.getSearchProducts();
-  setLocalStorage("search-results", data);
-  console.log(data);
+document
+  .querySelector("#search-button")
+  .addEventListener("click", async (e) => {
+    e.preventDefault();
+    const data = await dataSource.getSearchProducts();
+    setLocalStorage("search-results", data);
 
-  console.log(data[0].food.brand);
-});
+    //   console.log(data);
+    renderSearchResults();
+  });
 
 function renderSearchResults() {
-    const searchResults = getLocalStorage("search-results") || [];
-    const htmlItems = searchResults.map((item) => searchResultTemplate(item));
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const searchResults = getLocalStorage("search-results") || [];
+  const htmlItems = searchResults.map((item) => searchResultTemplate(item));
+  document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
 function searchResultTemplate(item) {
-    let image;
-    if (!item.food.image) {
-        image = "../images/filled-basket.jpg";
-    } else {
-        image = item.food.image;
-    }
-    const newItem = `<li class="result-card divider">
+  let image;
+  if (!item.food.image) {
+    image = "../images/filled-basket.jpg";
+  } else {
+    image = item.food.image;
+  }
+
+  let servingSize;
+  if (!item.food.servingsPerContainer) {
+    servingSize = "-";
+  } else {
+    servingSize = item.food.servingsPerContainer;
+  }
+  const newItem = `<li class="result-card divider">
         <a href="" class="result-card__image">
             <img
                 src="${image}"
@@ -40,12 +49,10 @@ function searchResultTemplate(item) {
             />
         </a>
         <a href="#">
-            <h2 class="card__name">${item.food.label}</h2>
+            <h2 class="card__name">${item.food.brand} ${item.food.label}</h2>
         </a>
-        <p class="result-card__serving">1</p>
+        <p class="result-card__serving">Serving Size: ${servingSize}</p>
         </li>`;
 
-        return newItem;
+  return newItem;
 }
-
-renderSearchResults();
